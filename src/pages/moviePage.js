442ccefbc0c0b9669991d845c398326a9d5fcd03
LMents <gm.campos@campus.fct.unl.pt>
2023-11-08 16:43:13 +0000

    ✨ initial movie page

---
+++ src/pages/moviePage.js
@@ -0,0 +1,18 @@
+import MyNavbar from '@/components/Navbar';
+
+function InitialLayout() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default function NearYouPage() {
+    return (
+        <div>
+            <MyNavbar />
+            <InitialLayout />
+        </div>
+    )
+}
