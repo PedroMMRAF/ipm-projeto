2ea039f58a5b3a6aeb366259790dff41a34a839e
PedroMMRAF <pmmr.fernandes@campus.fct.unl.pt>
2023-11-15 13:05:09 +0000

    aaaaaaaaa

--- src/pages/nearyou.js
+++ src/pages/nearyou.js
@@ -9,14 +9,20 @@ const center = [38.66004943847656, -9.203119277954102];
 const marker2 = [38.679087, -9.162256];
 
 export default function NearYouPage() {
+
+
+  const shrink = () => {
+
+  }
+
   return (
     <div>
       <MyNavbar />
-      <Container>
+      <div>
         <GoogleMapComponent center={center}>
           <CustomMarker color="blue" position={marker2} />
         </GoogleMapComponent>
-      </Container>
+      </div>
     </div>
   );
 }
