290f63d1b1e45fcadd990520044770c568d0e45a
joaosantos61045 <jpbr.santos@campus.fct.unl.pt>
2023-11-15 18:36:31 +0000

    Semi Usable Map

--- src/components/GoogleMapComponent.js
+++ src/components/GoogleMapComponent.js
@@ -2,9 +2,34 @@
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '500px',
-  height: '500px',
+  width: '100%',
+  height: '80vh',
 };
+const darkMapStyles = [{
+  "stylers": [{
+      "hue": "#ff1a00"
+  }, {
+      "invert_lightness": true
+  }, {
+      "saturation": -100
+  }, {
+      "lightness": 33
+  }, {
+      "gamma": 0.5
+  }]
+}, {
+  "featureType": "water",
+  "elementType": "geometry",
+  "stylers": [{
+      "color": "#2D333C"
+  }]},{
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [{
+      "visibility": 'off',
+    }]
+  
+}]
 
 export default function GoogleMapComponent({ center, children }) {
   return (
@@ -15,7 +40,11 @@ export default function GoogleMapComponent({ center, children }) {
           lat: center[0],
           lng: center[1],
         }}
-        zoom={10}
+        zoom={11}
+        options={{
+          styles: darkMapStyles,
+        }}
+       
       >
         {children}
       </GoogleMap>
