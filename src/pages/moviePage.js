984d380e7b2dda47164584a8cf0ab5fb85344c7f
LMents <72884774+GMFields@users.noreply.github.com>
2023-11-17 00:02:41 +0000

    🔥 added a bunch of new things undefined

--- src/pages/moviePage.js
+++ src/pages/moviePage.js
@@ -1,80 +1,113 @@
 import MyNavbar from '@/components/Navbar';
-import Container from 'react-bootstrap/Container';
+
+import React from 'react';
+import { useState } from 'react';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
-import React from 'react';
 import Card from 'react-bootstrap/Card';
-import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import style from "@/styles/moviePage.module.css"
+import Rating from '@mui/material/Rating'
+import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
+import TextField from '@mui/material/TextField';
+
+import PropTypes from "prop-types";
+
+import styles from "@/styles/moviePage.module.css"
 import MOVIES from "@/const/movies.json"
 
+const fs = require("fs")
+fs.readFileSync("@/const/movies.json")
 
+// The element containing everything within the movie background image
 function Headline() {
+
+    let rating = 0;
+
+    MOVIES[0]["reviews"].map((review, i) => (
+        rating += parseInt(review["rating"])
+    ))
+
+    rating = rating / MOVIES[0]["reviews"].length
+
     return (
-        <div className={style.div} style={{
-            backgroundImage: "url('https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/fm6KqXpk3M2HVveHwCrBSSBaO0V.jpg')"
+        <div className={styles.div} style={{
+            backgroundImage: `url(${MOVIES[0]["background-image"]})`
         }}>
-            <div className={style.cover} style={{ backgroundImage: "linear-gradient(to bottom right, rgba(31.5, 10.5, 10.5, 1), rgba(31.5, 10.5, 10.5, 0.84))" }}>
+            <div className={styles.cover} style={{ backgroundImage: MOVIES[0]["cover"] }}>
                 <Container>
                     <div>
                         <Row>
-                            <Col xs={4} md={4} className={style.content}>
+                            <Col xs={4} className={styles.content}>
                                 <div >
-                                    <Image src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg" style={{ height: "70vh" }} rounded />
+                                    <Image src={MOVIES[0]["poster"]} style={{ height: "70vh" }} rounded />
                                 </div>
                             </Col>
-                            <Col xs={8} className={style.content}>
-                                <div>
-                                    <div>
-                                        <h2>OppenHeimer</h2>
+                            <Col xs={8} className={styles.content} style={{ marginTop: "12vh" }}>
+                                <div style={{ marginLeft: "1vh" }}>
+                                    <div style={{ marginBottom: "3vh" }}>
+                                        <h2>{MOVIES[0]["title"]}</h2>
                                         <div>
-                                            <span className={style.span}>
+                                            <span className={styles.span}>
                                                 R
                                             </span>
-                                            <span className={style.span}>
+                                            <span className={styles.span}>
                                                 2023 US
                                             </span>
-                                            <span className={style.span}>
-                                                Drama, History
+                                            <span className={styles.span}>
+                                                &#x2022; Drama, History
                                             </span>
-                                            <span className={style.span}>
-                                                3h 1m
+                                            <span className={styles.span}>
+                                                &#x2022; 3h 1m
                                             </span>
                                         </div>
                                     </div>
-                                    <Row>
-                                        <Col sm={2}>
+                                    <div style={{ marginLeft: "-2vw" }}>
+                                        <Row>
 
-                                        </Col>
-                                        <Col xs={2}>
-                                            <i class="bi bi-play"></i> Play Trailer
-                                        </Col>
-                                        <Col xs={3}>
-                                            <i class="bi bi-bookmark-plus"></i> Add To Watchlist
-                                        </Col>
-                                        <Col>
-                                            <ModalPage />
-                                        </Col>
-                                    </Row>
+                                            <Col xs={3}>
+                                                <svg style={{ height: "8vh", width: "8vw", color: "yellow" }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="ipc-icon ipc-icon--star sc-bde20123-4 frBGmx" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72 3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"></path></svg>
+                                                <span style={{ marginLeft: "-1.5vw" }}>
+                                                    {rating}/5
+                                                </span>
+                                            </Col>
+                                            <Col xs={3}>
+                                                <TrailerModal />
+                                            </Col>
+                                            <Col xs={3}>
+                                                <div style={{ marginTop: "2vh", marginLeft: "-5vw", cursor: "pointer" }}>
+                                                    <i class="bi bi-bookmark-plus"></i> Add To Watchlist
+                                                </div>
+                                            </Col>
+                                            <Col xs={3}>
+                                                <ReviewModal />
+                                            </Col>
+                                        </Row>
+                                    </div>
                                     <div>
-                                        <h3>The world forever changes</h3>
-                                        <h3>Overview</h3>
+                                        <h3 style={{ fontSize: "18px", color: "grey", marginBottom: "3vh", marginTop: "2vh" }}><i>{MOVIES[0]["quote"]}</i></h3>
+                                        <h3 style={{ fontSize: "20px" }}>Overview</h3>
                                         <div>
                                             <p>
-                                                The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.
+                                                {MOVIES[0]["overview"]}
                                             </p>
                                         </div>
-                                        <Container>
+                                        <Container style={{ marginLeft: "-0.7vw" }}>
                                             <Row>
-                                                <p>Christopher Nolan</p>
-                                                <p>Director, Writer</p>
+                                                {
+                                                    MOVIES[0]["colab"].map((colaborator, i) => (
+                                                        <div><p><b>Christopher Nolan</b></p>
+                                                            <p style={{ marginTop: "-3vh", fontSize: "14px" }}>{colaborator["roles"]}</p></div>
+                                                    ))}
                                             </Row>
                                         </Container>
                                     </div>
@@ -88,6 +121,7 @@ function Headline() {
     )
 }
 
+// Left side of the page minus headline
 function Body() {
     return (
         <div>
@@ -95,91 +129,156 @@ function Body() {
                 <h3>Top Actors</h3>
                 <SimpleSlider />
             </div>
-            <hr className={style.hr} />
-            <div>
+            <hr className={styles.hr} />
+            <div style={{ overflowY: "auto", maxHeight: "90vh" }}>
                 {MOVIES[0]["reviews"].map((review, i) => (
-                    <p>{review["review"]}</p>
+                    <ReviewCard author={review["author"]} review={review["review"]} rating={review["rating"]} image={review["profile-image"]} />
                 ))}
             </div>
-        </div>
+        </div >
     )
 }
 
-
-function Actor() {
+function ReviewCard({ author, review, rating, image }) {
     return (
-        <Card className={style.card}>
+        <Card style={{ marginTop: "2vh" }}>
+            <Card.Header><Image src={image} style={{ width: "2.5vw", height: "5vh", borderRadius: "50%", marginRight: "1vw" }} />  <b style={{ fontSize: "110%" }}>A review by {author} </b></Card.Header>
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Title><Chip label={<i class="bi bi-star-fill"><span style={{ marginLeft: "0.5vw" }}>{rating}</span></i>} /></Card.Title>
                 <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    {review}
                 </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
         </Card>
     );
 }
 
+// Card containing each actor
+function Actor({ name, character, image }) {
+    return (
+        <Card className={styles.card}>
+            <Card.Img variant="top" src={image} style={{ height: "25vh" }} />
+            <Card.Body>
+                <Card.Title className={styles.title}>{name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted" style={{ fontSize: "small" }}>{character}</Card.Subtitle>
+            </Card.Body>
+        </Card>
+    );
+}
+
+// Right side of the body minus headline
 function Sidebody() {
     return (
         <div>
             <Container>
-                <p>
-                    <strong>
-                        <bdi>Status</bdi>
-                    </strong>
-                    Released
-                </p>
-                <p>
-                    <strong>
-                        <bdi>Original Language</bdi>
-                    </strong>
-                    English
-                </p>
-                <p>
-                    <strong>
-                        <bdi>Budget</bdi>
-                    </strong>
-                    $100,000,000.00
-                </p>
-                <p>
-                    <strong>
-                        <bdi>Revenue</bdi>
-                    </strong>
-                    $948,900,000.00
-                </p>
+                <div>
+                    <p>
+                        <strong>
+                            <bdi>Status</bdi>
+                        </strong>
+                        <p>{MOVIES[0]["status"]}</p>
+                    </p>
+                    <p>
+                        <strong>
+                            <bdi>Original Language</bdi>
+                        </strong>
+                        <p>{MOVIES[0]["original-language"]}</p>
+                    </p>
+                    <p>
+                        <strong>
+                            <bdi>Budget</bdi>
+                        </strong>
+                        <p>{MOVIES[0]["budget"]}</p>
+                    </p>
+                    <p>
+                        <strong>
+                            <bdi>Revenue</bdi>
+                        </strong>
+                        <p>{MOVIES[0]["revenue"]}</p>
+                    </p>
+                </div>
             </Container>
         </div>
     )
 }
 
+// Slider arrow settings
+function SliderArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={`${className} ${style.arrow}`}
+            style={{ background: "black" }}
+            onClick={onClick}
+        />
+    );
+}
 
-function ModalPage() {
+// Slider with actors
+function SimpleSlider() {
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        nextArrow: <SliderArrow />,
+        prevArrow: <SliderArrow />
+    }
+
+    return (
+        <Slider {...settings}>
+            {MOVIES[0]["actors"].map((actor, i) => (
+                <Actor name={actor["name"]} character={actor["character"]} image={actor["image"]} />
+            ))}
+        </Slider>
+    );
+}
+
+// Modal of the review
+function ReviewModal() {
     const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
+    const [value, setValue] = useState(0);
+
+    const handleClose = () => {
+        setShow(false)
+        let review = document.getElementById("textarea").value
+        console.log()
+        MOVIES[0]["reviews"].push({
+            "profile-image": "https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+            "author": "eu",
+            "review": review,
+            "rating": value
+        })
+    };
     const handleShow = () => setShow(true);
+
 
     return (
         <>
-            <div variant="primary" onClick={handleShow}>
+            <div variant="primary" onClick={handleShow} style={{ marginTop: "2vh", marginLeft: "-5vw", cursor: "pointer" }}>
                 <i class="bi bi-star"></i> Review
             </div>
-
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title style={{ fontFamily: "'Source Sans Pro',Arial,sans-serif" }}>Rate {MOVIES[0]["title"]}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                <Modal.Body>
+                    <Typography component="legend">Give us your opinion</Typography>
+                    <Rating
+                        name="simple-controlled"
+                        value={value}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                        }}
+                    />
+                    <p></p>
+                    <textarea id='textarea' style={{ width: "100%" }} />
+                </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
                     <Button variant="primary" onClick={handleClose}>
-                        Save Changes
+                        Review
                     </Button>
                 </Modal.Footer>
             </Modal>
@@ -187,36 +286,48 @@ function ModalPage() {
     );
 }
 
+function TrailerModal() {
+    const [show, setShow] = useState(false);
 
-function SimpleSlider() {
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 2,
-        slidesToScroll: 2
-    };
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <Slider {...settings}>
-            <div>
-                <h3>1</h3>
+        <>
+            <div onClick={handleShow} style={{ marginTop: "2vh", marginLeft: "-2vw", cursor: "pointer" }}>
+                <i class="bi bi-play"></i> Play Trailer
             </div>
-            <div>
-                <h3>2</h3>
-            </div>
-            <div>
-                <h3>3</h3>
-            </div>
-            <div>
-                <h3>4</h3>
-            </div>
-            <div>
-                <h3>5</h3>
-            </div>
-            <div>
-                <h3>6</h3>
-            </div>
-        </Slider>
+            <Modal show={show} onHide={handleClose} size='lg' style={{ display: "flex" }}>
+                <Modal.Body>
+                    <Player />
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+}
+
+function Player() {
+
+    const YoutubeEmbed = ({ embedId }) => (
+        <div className="video-responsive">
+            <iframe style={{ height: "80vh", width: "100%" }}
+                src={`https://www.youtube.com/embed/${embedId}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+            />
+        </div>
+    );
+
+    YoutubeEmbed.propTypes = {
+        embedId: PropTypes.string.isRequired
+    };
+
+    return (
+        <div>
+            <YoutubeEmbed embedId={MOVIES[0]["trailer"]} />
+        </div>
     );
 }
 
@@ -227,10 +338,10 @@ export default function NearYouPage() {
             <Headline />
             <Container>
                 <Row>
-                    <Col md={8}>
+                    <Col md={9} sm={9}>
                         <Body />
                     </Col>
-                    <Col className={style.sidebody}>
+                    <Col className={styles.sidebody} md={3} sm={3}>
                         <Sidebody />
                     </Col>
                 </Row>
