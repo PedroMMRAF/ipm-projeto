20be5a19154f1d82c6383d407716756407067357
AntiMach <themachinumps@gmail.com>
2023-11-21 20:38:31 +0000

    new pdf

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 window.addEventListener("load", () => {
     [...document.getElementsByTagName("button")]
-        .filter((element) => (element.attributes["data-file"] !== undefined))
         .forEach((element) => {
-            console.log(element);
+            if (!element.attributes["data-pdf-modal"]) return;
+            const file = element.attributes["data-pdf-modal"].value;
+
+            const target = file.replace(".pdf", "").replace(/\/|\s/g, "-") + "-modal";
+
+            element.setAttribute("data-bs-toggle", "modal");
+            element.setAttribute("data-bs-target", "#" + target);
 
             const div = document.createElement("div");
-            div.setAttribute("id", element.attributes["data-bs-target"].value.replace("#", ""));
+            div.setAttribute("id", target);
             div.setAttribute("class", "modal fade");
             div.setAttribute("tabindex", "-1");
             div.innerHTML = `
@@ -16,8 +21,8 @@ window.addEventListener("load", () => {
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body m-0 p-0">
-                            <object data="${element.attributes["data-file"].value}" type="application/pdf" width="100%" height="100%">
-                                <a href="${element.attributes["data-file"].value}">View PDF</a>
+                            <object data="${file}" type="application/pdf" width="100%" height="100%">
+                                <a href="${file}">View PDF</a>
                             </object>
                         </div>
                     </div>
